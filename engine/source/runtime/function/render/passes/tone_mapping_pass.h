3c147cf1d1e884b54d7eb0ebfae919a9bbe5bb7d
Ttoxicx <1908087005@qq.com>
2024-08-15 11:38:51 +0800

    render

---
+++ engine/source/runtime/function/render/passes/tone_mapping_pass.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "runtime/function/render/render_pass.h"
+
+namespace Dao {
+	
+	struct ToneMappingPassInitInfo :RenderPassInitInfo {
+		RHIRenderPass* render_pass;
+		RHIImageView* input_attachment;
+	};
+
+	class ToneMappingPass :public RenderPass {
+	public:
+		void initialize(const RenderPassInitInfo* init_info) override final;
+		void draw() override final;
+
+		void updateAfterFramebufferRecreate(RHIImageView* input_attachment);
+
+	private:
+		void setupDescriptorSetLayout();
+		void setupPipelines();
+		void setupDescriptorSet();
+	};
+}
