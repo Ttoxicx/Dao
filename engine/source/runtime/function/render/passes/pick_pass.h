e8a028f4b014f7af4238507b15d42674984abd71
Ttoxicx <1908087005@qq.com>
2024-08-16 20:20:51 +0800

    render

---
+++ engine/source/runtime/function/render/passes/pick_pass.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "runtime/core/math/vector2.h"
+#include "runtime/function/render/render_pass.h"
+
+namespace Dao {
+
+	class RenderResourceBase;
+
+	struct PickPassInitInfo :RenderPassInitInfo {
+		RHIDescriptorSetLayout* per_mesh_layout;
+	};
+
+	class PickPass :public RenderPass {
+	public:
+		void initialize(const RenderPassInitInfo* init_info) override final;
+		void postInitialize() override final;
+		void preparePassData(std::shared_ptr<RenderResourceBase> render_resource) override final;
+		void draw() override final;
+
+		uint32_t pick(const Vector2& pick_uv);
+		void recreateFramebuffer();
+
+	private:
+		void setupAttachments();
+		void setupRenderPass();
+		void setupFramebuffer();
+		void setupDescriptorSetLayout();
+		void setupPipelines();
+		void setupDescriptorSet();
+
+	private:
+		RHIImage* _object_id_image = nullptr;
+		RHIImageView* _object_id_image_view = nullptr;
+		RHIDeviceMemory* _object_id_mage_memory = nullptr;
+		RHIDescriptorSetLayout* _per_mesh_layout = nullptr;
+		MeshInefficientPickPerframeStorageBufferObject _mesh_inefficient_pick_perframe_storage_buffer_object;
+	};
+}
